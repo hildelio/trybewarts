0bed8f754242680a2af82a37592659e5b2aa3dd1
Victor <victordiricoo@gmail.com>
2022-07-13 16:58:07 -0300

    arrumando o lint

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const login = document.querySelector('.trybewarts-login');
-login.addEventListener('submit', function(event) {
-  if (document.getElementById('login').value === 'tryber@teste.com' && document.getElementById('senha').value === '123456') {
+const emailLogin = document.getElementById('login');
+const senhaLogin = document.getElementById('senha');
+login.addEventListener('submit', (event) => {
+  if (emailLogin.value === 'tryber@test.com' && senhaLogin.value === '123456') {
     event.preventDefault();
-    alert('Olá, Tryber!');
+    alert('Olá Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
@@ -17,55 +19,56 @@ function ableDisableButton() {
     button.disabled = true;
   }
 }
+ableDisableButton();
 
-let count = 500;
-let counter = document.getElementById('counter');
-const textarea = document.querySelector('#textarea');
-textarea.addEventListener('keypress', function(event) {
-count = 500 - (textarea.value.length + 1);
-counter.innerText = count + ' caracteres restantes.';
-});
+// let count = 500;
+// let counter = document.getElementById('counter');
+// const textarea = document.querySelector('#textarea');
+// textarea.addEventListener('keypress', function(event) {
+// count = 500 - (textarea.value.length + 1);
+// counter.innerText = count + ' caracteres restantes.';
+// });
 
-const form = document.querySelector('#evaluation-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const name = document.getElementById('input-name');
-  const lastname = document.getElementById('input-lastname');
-  const email = document.getElementById('email');
-  const house = document.getElementById('house'); //confirmar se é só isso mesmo
-  const family = document.querySelector('input[name = "family"]:checked');
-  const content = document.querySelectorAll('input[name = "content"]:checked');
-  const rate = document.querySelector('input[name = "rate":checked');
-  const textarea = document.getElementById('textarea');
+// const form = document.querySelector('#evaluation-form');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   const name = document.getElementById('input-name');
+//   const lastname = document.getElementById('input-lastname');
+//   const email = document.getElementById('email');
+//   const house = document.getElementById('house'); //confirmar se é só isso mesmo
+//   const family = document.querySelector('input[name = "family"]:checked');
+//   const content = document.querySelectorAll('input[name = "content"]:checked');
+//   const rate = document.querySelector('input[name = "rate":checked');
+//   const textarea = document.getElementById('textarea');
 
-  const object = [
-    {
-      question: '-Nome- -Sobrenome-',
-      answer: `${name.value} ${lastname.value}`  
-    },
-    {
-      question: 'Email',
-      answer: email.value,
-    },
-    {
-      question: '-Casa-',
-      answer: house.value,
-    },
-    {
-      question: '-Família-',
-      answer: `${family.value}`,
-    },
-    {
-      question: '-Matérias Selecionadas-',
-      answer: `${content.value}`,
-    },
-    {
-      question: '-Avaliação-',
-      answer: `${rate.value}`,
-    },
-    {
-      question: '-Observações-',
-      answer: textarea.value,
-    }
-  ]
-});
+//   const object = [
+//     {
+//       question: '-Nome- -Sobrenome-',
+//       answer: `${name.value} ${lastname.value}`
+//     },
+//     {
+//       question: 'Email',
+//       answer: email.value,
+//     },
+//     {
+//       question: '-Casa-',
+//       answer: house.value,
+//     },
+//     {
+//       question: '-Família-',
+//       answer: `${family.value}`,
+//     },
+//     {
+//       question: '-Matérias Selecionadas-',
+//       answer: `${content.value}`,
+//     },
+//     {
+//       question: '-Avaliação-',
+//       answer: `${rate.value}`,
+//     },
+//     {
+//       question: '-Observações-',
+//       answer: textarea.value,
+//     }
+//   ]
+// });
