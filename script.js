316bb1771c289d295dcd96ee1df19b47433ec4b6
hildelio <hildelio@gmail.com>
2022-07-13 15:57:51 -0300

    requisitos de 1 a 12 concluidos

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const login = document.querySelector('.trybewarts-login');
 login.addEventListener('submit', function(event) {
-  if (document.getElementById('login').value === 'tryber@test.com' && document.getElementById('senha').value === '123456') {
+  if (document.getElementById('login').value === 'tryber@teste.com' && document.getElementById('senha').value === '123456') {
     event.preventDefault();
-    alert('Olá Tryber!');
+    alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
